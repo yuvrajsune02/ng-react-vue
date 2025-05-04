bbc6a9651f849e729ee41e5e2ac326f09d5a539c
Yuvraj Sune <yuvrajsune02@gmail.com>
2025-05-04 22:45:13 +0530

    init with react and vue

---
+++ apps/react-demo/src/app/app.tsx
@@ -0,0 +1,15 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-demo"/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ apps/vue-demo/src/app/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import NxWelcome from './NxWelcome.vue';
+</script>
+
+<template>
+  <NxWelcome title="@ng-react-vue/vue-demo" />
+</template>
+
